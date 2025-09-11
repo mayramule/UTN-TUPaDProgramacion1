efb23b3fe87c157538749b417f1ea1643506158d
Mayra Mulé <mairamule.mm@gmail.com>
2025-09-11 14:56:07 -0300

    Subo ejercicio 3

--- 03 Estructuras Condicionales/Mule_Practico 3.py
+++ 03 Estructuras Condicionales/Mule_Practico 3.py
@@ -15,3 +15,15 @@ if nota >= 6:
     print("Aprobado")
 else: 
     print("Desaprobado")
+
+
+# Ejercicio 3
+# Escribir un programa que permita ingresar solo números pares. Si el usuario ingresa un
+# número par, imprimir por en pantalla el mensaje "Ha ingresado un número par"; en caso
+# contrario, imprimir por pantalla "Por favor, ingrese un número par". Nota: investigar el uso del
+# operador de módulo (%) en Python para evaluar si un número es par o impar.
+numero = int(input("Ingrese un numero a continuacion: "))
+if numero % 2 == 0:
+    print(f"Ha ingreado un numero par: {numero}") 
+else:
+    print("Por favor ingrese un numero par.")
