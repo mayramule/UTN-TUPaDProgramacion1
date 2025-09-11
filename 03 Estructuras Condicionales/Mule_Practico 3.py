71aeec9f30558741e50d0c9604c0a1f0ce12867a
Mayra Mulé <mairamule.mm@gmail.com>
2025-09-11 15:22:49 -0300

    Subo ejercicio 4

--- 03 Estructuras Condicionales/Mule_Practico 3.py
+++ 03 Estructuras Condicionales/Mule_Practico 3.py
@@ -27,3 +27,21 @@ if numero % 2 == 0:
     print(f"Ha ingreado un numero par: {numero}") 
 else:
     print("Por favor ingrese un numero par.")
+
+
+# Ejercicio 4
+# Escribir un programa que solicite al usuario su edad e imprima por pantalla a cuál de las
+# siguientes categorías pertenece:
+# Niño/a: menor de 12 años.
+# Adolescente: mayor o igual que 12 años y menor que 18 años.
+# Adulto/a joven: mayor o igual que 18 años y menor que 30 años.
+# Adulto/a: mayor o igual que 30 años.
+edad = int(input("Ingrese su edad para saber a que categoría pertenece: "))
+if edad < 12: 
+    print("Niño/a menor de 12 años")
+elif edad >= 12 and edad < 18:
+    print("Adolescente: mayor o igual que 12 años y menor que 18 años.")
+elif edad >= 18 and edad < 30:
+    print("Adulto/a joven: mayor o igual que 18 años y menor que 30 años.")
+else:
+    print("Adulto/a: mayor o igual que 30 años.")
