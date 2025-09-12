faaa9a1a0e9adf3353f5f9df000354f7dcb380cd
Mayra Mulé <mairamule.mm@gmail.com>
2025-09-12 14:16:48 -0300

    Subo ejercicio 7

--- 03 Estructuras Condicionales/Mule_Practico 3.py
+++ 03 Estructuras Condicionales/Mule_Practico 3.py
@@ -86,3 +86,16 @@ print(f"Lista de numeros aleatorios: {numeros_aleatorios}")
 print(f"Media: {media}")
 print(f"Moda: {moda}")
 print(f"Mediana: {mediana}")
+
+
+# Ejercicio 7
+#Escribir un programa que solicite una frase o palabra al usuario. Si el string ingresado
+#termina con vocal, añadir un signo de exclamación al final e imprimir el string resultante por
+#pantalla; en caso contrario, dejar el string tal cual lo ingresó el usuario e imprimirlo por
+#pantalla.
+frase = input("Ingrese una frase o una palabra a continuacion:")
+if frase[-1].lower() in "aeiou":
+    respuesta = frase + "!"
+else:
+    respuesta =  frase
+print(f"Su frase o palabra es: {respuesta}")
