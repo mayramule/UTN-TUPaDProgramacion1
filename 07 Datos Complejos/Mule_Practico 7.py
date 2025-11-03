9db42a2e7e36a426ee053147379cf76a69b460dc
Mayra Mulé <mairamule.mm@gmail.com>
2025-11-02 21:16:42 -0300

    Correccion TP 7

--- 07 Datos Complejos/Mule_Practico 7.py
+++ 07 Datos Complejos/Mule_Practico 7.py
@@ -176,8 +176,6 @@ while True:
 
 '''9) Creá una agenda donde las claves sean tuplas de (día, hora) y los valores sean eventos.
 Permití consultar qué actividad hay en cierto día y hora.'''
-
-# 1. Crear el diccionario 'agenda' donde las claves son tuplas (dia, hora)
 agenda = {
     ("lunes", "10:00"): "Reunión de planificación",
     ("martes", "15:00"): "Clase de inglés",
